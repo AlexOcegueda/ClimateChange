221a05848b07ada28bd1a3013507b33c75831571
Alex Ocegueda Castro <alexocegueda59@gmail.com>
2024-07-17 18:05:13 -0500

    Fixed database routes

--- climate-change-website/src/app/api/[table]/route.ts
+++ climate-change-website/src/app/api/[table]/route.ts
@@ -3,22 +3,30 @@ import sqlite3 from 'sqlite3';
 import path from 'path';
 
 const isVercel = process.env.VERCEL_ENV !== undefined;
-const dbPath = isVercel ? path.join('/var/task/database', 'climate_change.db') : path.join(process.cwd(), 'database', 'climate_change.db');
-const db = new sqlite3.Database(dbPath);
+const dbPath = isVercel ? path.join(process.cwd(), 'database/climate_change.db') : path.join(process.cwd(), '../database/climate_change.db');
 
-export async function GET(request: Request, { params }: { params: { table: string } }) {
-  const { table } = params;
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Database connection failed: ", err);
+    } else {
+        console.log("Connected to the database.");
+    }
+});
 
-  if (!table) {
-    return NextResponse.json({ error: 'Table name is required' }, { status: 400 });
-  }
+export async function GET(request: Request, { params }: { params: { table: string } }): Promise<Response> {
+    const { table } = params;
 
-  return new Promise((resolve, reject) => {
-    db.all(`SELECT * FROM ${table}`, [], (err, rows) => {
-      if (err) {
-        reject(NextResponse.json({ error: err.message }, { status: 500 }));
-      }
-      resolve(NextResponse.json({ data: rows }, { status: 200 }));
+    if (!table) {
+        return NextResponse.json({ error: 'Table name is required' }, { status: 400 });
+    }
+
+    return new Promise((resolve) => {
+        db.all(`SELECT * FROM ${table}`, [], (err, rows) => {
+            if (err) {
+                resolve(NextResponse.json({ error: err.message }, { status: 500 }));
+            } else {
+                resolve(NextResponse.json({ data: rows }, { status: 200 }));
+            }
+        });
     });
-  });
 }
--- climate-change-website/src/app/api/co2statistics/route.ts
+++ climate-change-website/src/app/api/co2statistics/route.ts
@@ -3,8 +3,15 @@ import sqlite3 from 'sqlite3';
 import path from 'path';
 
 const isVercel = process.env.VERCEL_ENV !== undefined;
-const dbPath = isVercel ? path.join('/var/task/database', 'climate_change.db') : path.join(process.cwd(), 'database', 'climate_change.db');
-const db = new sqlite3.Database(dbPath);
+const dbPath = isVercel ? path.join(process.cwd(), 'database/climate_change.db') : path.join(process.cwd(), '../database/climate_change.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Database connection failed: ", err);
+    } else {
+        console.log("Connected to the database.");
+    }
+});
 
 export async function GET() {
   return new Promise((resolve, reject) => {
