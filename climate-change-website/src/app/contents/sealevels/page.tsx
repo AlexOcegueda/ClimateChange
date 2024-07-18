6a234f54fa5c87ed6fda6f2fbef3502ff504a717
Alex Ocegueda Castro <alexocegueda59@gmail.com>
2024-07-17 20:00:31 -0500

    Fixed mobile responsive css

--- climate-change-website/src/app/contents/sealevels/page.tsx
+++ climate-change-website/src/app/contents/sealevels/page.tsx
@@ -12,12 +12,12 @@ const SeaLevelsTrendsPage = () => {
       <div className="w-full max-w-full mx-auto text-center flex-grow">
         <div className="relative flex justify-center mb-8">
           <div className="ml-4 text-[2rem] sm:text-[3rem] md:text-[5rem] font-bold text-[#b2c3ee] z-10 bg-white p-4">02</div>
-          <div className="absolute left-0 right-0 bg-white h-full -z-10" style={{ height: '3rem', sm: { height: '4rem' }, md: { height: '6rem' }}}></div>
+          <div className="absolute left-0 right-0 bg-white -z-10 h-12 sm:h-16 md:h-24"></div>
           <h1 className="ml-4 text-[2rem] sm:text-[3rem] md:text-[5rem] font-bold text-[#b2c3ee] z-10 bg-white flex-grow flex items-center pl-5">SEA LEVELS</h1>
         </div>
         <main className="bg-white p-8 mx-auto shadow-lg rounded-lg flex-grow" style={{ maxWidth: '60rem', marginLeft: 'auto', marginRight: 'auto' }}>
           <blockquote className="text-3xl text-gray-500 mb-8">
-            &quot;How has global sea levels changed over the past century&quot;
+            &quot;How have global sea levels changed over the past century&quot;
           </blockquote>
           <h2 id="why-sea-levels" className="text-2xl font-bold mb-4">Why Sea Levels?</h2>
           <p className="text-left text-lg mb-4">
