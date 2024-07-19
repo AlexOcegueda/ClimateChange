a006b327ff9cd376d982aba4612246f6a4428c8c
Alex Ocegueda Castro <alexocegueda59@gmail.com>
2024-07-18 22:21:59 -0500

    Removed dynamic routing and replaced with static only

---
+++ climate-change-website/src/app/api/LandOceanGlobalMean/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+const dbPath = process.env.DATABASE_PATH || path.join(process.cwd(), 'database/climate_change.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Database connection failed: ", err);
+    } else {
+        console.log("Connected to the database.");
+    }
+});
+
+export async function GET(): Promise<Response> {
+  return new Promise((resolve, reject) => {
+    db.all(`
+      SELECT 
+        * 
+      FROM LandOceanGlobalMean
+    `, [], (err, rows) => {
+      if (err) {
+        reject(NextResponse.json({ error: err.message }, { status: 500 }));
+      } else {
+        resolve(NextResponse.json({ data: rows }, { status: 200 }));
+      }
+    });
+  });
+}
