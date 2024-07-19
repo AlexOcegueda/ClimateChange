1b686009625b2fb23c74dc94321925ec4755e7bf
Alex Ocegueda Castro <alexocegueda59@gmail.com>
2024-07-19 11:46:41 -0500

    Fixed className error

--- climate-change-website/src/components/NavBar.tsx
+++ climate-change-website/src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
   return (
     <header className="flex justify-between items-center p-4 bg-transparent z-20 relative">
       <nav className="space-x-4">
-        <img class="h-[50px] w-auto" src="/assets/earth_logo.png" alt="home" /></nav>
+        <img className="h-[50px] w-auto" src="/assets/earth_logo.png" alt="home" /></nav>
       <nav className="space-x-4 flex">
         <Link href="https://ocegueda.dev/" className="nav-link text-lg font-semibold text-white">portfolio</Link>
         <Link href="https://github.com/AlexOcegueda/ClimateChange" className="nav-link text-lg font-semibold text-white">github</Link>
